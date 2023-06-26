7f948e4dfc0a174fe312566e38cb94c812441467
Paolo96700 <paolo.falcoapp@gmail.com>
2023-06-26 20:38:30 +0200

    bonus

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -6,14 +6,20 @@
     <div class="row g-3 row-cols-3">
         @foreach ($arrMovies as $movie)
             <div class="col">
-                <div class="card h-100">
+                <div class="card rounded-4 h-100">
                     {{-- <img src="{{ Vite::asset('resources/img/' . $movie->img) }}" class="card-img-top" alt="{{ $book->title }}"> --}}
-                    <div class="card-body">
-                    <h5 class="card-title">{{ $movie->title }}</h5>
-                    <p class="card-text"> <span style="font-weight: 700">Date: </span>{{ $movie->date }}</p>
-                    {{-- <a href="#" class="btn btn-primary">Go somewhere</a> --}}
+                    <img src="{{ Vite::asset('resources/img/' . $movie->image) }}" class="h-100 image-fluid rounded-4" alt="">
+
+                    <div class="information">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $movie->title }}</h5>
+                            <p class="card-text"> <span style="font-weight: 700">Date: </span>{{ $movie->date }}</p>
+                            {{-- <a href="#" class="btn btn-primary">Go somewhere</a> --}}
+                        </div>
                     </div>
+                    
                 </div>
+                
             </div>
         @endforeach
     </div>
